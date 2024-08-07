3c741ed310ab68f387db800ff201f4fa9d34abf4
Y********* <y*********@gmail.com>
2024-08-07 16:13:15 +0530

    Setup the project  building the firstmicroservice

---
+++ eshop-microservices/Catalog/Products/CreateProduct/CreateProductEndpoints.cs
@@ -0,0 +1,16 @@
+﻿using BuildingBlocks.CQRS;
+using Carter;
+
+namespace Catalog.Api.Products.CreateProduct
+{
+    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
+    public record CreateProductResponse(Guid id);
+    public class CreateProductEndpoints : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+           // app.MapPost("/products", async(CreateProductRequest request))
+            throw new NotImplementedException();
+        }
+    }
+}
